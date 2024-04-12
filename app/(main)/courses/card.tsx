71761162026583adf5dfd1059c617e6363fee48c
YeSuX <351490949@qq.com>
2024-04-12 16:41:52 +0800

    feat: courses page

---
+++ app/(main)/courses/card.tsx
@@ -0,0 +1,46 @@
+import { cn } from "@/lib/utils";
+import { Check, Flag } from "lucide-react";
+
+type Props = {
+  title: string;
+  id: number;
+  imageSrc: string;
+  onClick: (id: number) => void;
+  disabled?: boolean;
+  active?: boolean
+}
+
+
+const Card = ({
+  title,
+  id,
+  imageSrc,
+  disabled,
+  onClick,
+  active
+}: Props) => {
+  return (
+    <div
+      onClick={() => onClick(id)}
+      className={cn(
+        'h-full border-2 rounded-xl border-b-4 hover:bg-black/5 cursor-pointer active:border-b-2 flex flex-col items-center justify-between p-3 pb-6 min-h-[217px] min-w-[200px]',
+        disabled && 'pointer-events-none opacity-50'
+      )}
+    >
+      <div className="min-[24px] w-full flex items-center justify-end">
+        {
+          active && (
+            <div className=" rounded-md bg-orange-600 flex items-center">
+              <Check className="text-white stroke-[4] h-4 w-4" />
+            </div>
+          )
+        }
+      </div>
+      <Flag className=" w-10 h-10 rounded-lg drop-shadow-md border object-cover" />
+      <p className=" text-neutral-700 text-center font-bold mt-3">
+        {title}
+      </p>
+    </div>
+  )
+}
+export default Card
--- db/schema.ts
+++ db/schema.ts
@@ -1,7 +1,7 @@
 import { pgTable, serial, text } from "drizzle-orm/pg-core";
 
 export const courses = pgTable('courses', {
-  id: serial('id').primaryKey(),
+  id: serial('id').notNull().primaryKey(),
   title: text('title').notNull(),
   imageSrc: text('image_src').notNull()
 })
